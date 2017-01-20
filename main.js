1a98ca784eb6680fbaea673a5a593f8cd7d4a79a
AshvinPainiaye <ash-concept@hotmail.fr>
2017-01-20 11:12:18 +0400

    maj

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({width: 800, height: 600, icon: __dirname + '/favicon.ico'})
 
   // and load the index.html of the app.
   win.loadURL(url.format({
